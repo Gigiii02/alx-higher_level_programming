4870a0f5a3add87578785658963037a536aaa907
Gigiii02 <giovannajames02@gmail.com>
2023-12-19 12:27:32 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Square module."""
 
-class square:
-    """Define a square."""
+class Square:
+    """Defines a square."""
 
     def __init__(self, size=0):
         """Constructor.
 
         Args:
-        size: length of a side of the square.
+        size: Length of a side of the square.
 
         Raises:
         TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
+        ValueError: If size is less than 0
         """
-        if not ininstance(size, int):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
