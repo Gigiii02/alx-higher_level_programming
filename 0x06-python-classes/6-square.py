69bb83afd0185c5d84a0943e86d408b9f28393ba
Gigiii02 <giovannajames02@gmail.com>
2023-12-19 12:43:49 -0800

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Define a class square."""
 
-class square:
+class Square:
     """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
@@ -20,7 +20,7 @@ class square:
 
         @size.setter
         def size(self, value):
-            if not isinstance(valye, int):
+            if not isinstance(value, int):
                 raise TypeError("size must be an integer")
             elif value < 0:
                 raise ValueError("size must be >= 0")
