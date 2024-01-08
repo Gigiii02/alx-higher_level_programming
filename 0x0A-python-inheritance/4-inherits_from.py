a942b5524ffad41799bcc604708f9a1b1b63ede5
Gigiii02 <giovannajames02@gmail.com>
2024-01-08 04:50:53 -0800

    a

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+def inherits_from(obj, a_class):
+    '''determines if an object is a true sublcass of a class.'''
+    return isinstance(obj, a_class) and type(objecr) != a_class
